ab1d1150c8ed57b31764a13c2a2d0bb457212c4f
Robert Mafabi <robertmafabi777@gmail.com>
2025-03-25 18:09:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+let companyName = "Scuber";
+{
+    const companyName = "Scuber";
+}
+const mostProfitableNeighborhood = "Chelsea";
+{
+    let mostProfitableNeighborhood = "Chelsea";
+}
+const companyCeo = "Susan Smith";
+{
+    let companyCeo = "Susan Smith";
+}
 // Code your solution in this file!
